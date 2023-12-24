781a372de6efce6d44a6301078328ab4618eba8d
Samuele Santi <samuele@samuelesanti.com>
2023-12-24 13:47:52 -0500

    Pure javascript version, make it prettier also

--- Makefile
+++ Makefile
@@ -1,9 +1,6 @@
 .PHONY: all publish
 
-all: src/index.html
-
-src/index.html: bin/generate_markup.py
-	python bin/generate_markup.py > $@
+all:
 
 publish:
 	ghp-import -n -p ./src/
--- src/falling-words.js
+++ src/falling-words.js
@@ -1,32 +1,65 @@
 (function() {
 
+    // Name of the class we add to words once they have been hovered.
+    // This makes them "disappear" from the page, while still filling up space.
     const HOVERED_CLASS_NAME = "hovered";
 
-    document.querySelectorAll(".word").forEach(
-	word => word.addEventListener("mouseenter", event => {
+    // Get thext from the <script id="source-text"> tag
+    const rawText = document.getElementById("source-text").textContent;
+    const textContainer = document.getElementById("text");
+
+    // Render the "plain" text obtained earlier into the appropriate markup for the animation.
+    // Each line becomes a paragraph, and each word is wrapped in a <span class="word">.
+    renderTextMarkup(rawText, textContainer);
+
+    // Attach event handlers to each <span class="word"> tag
+    textContainer.querySelectorAll(".word").forEach(word => {
+
+	// On mouse hover, hide the word and create a falling "clone"
+	word.addEventListener("mouseenter", event => {
 	    let el = event.target;
 
 	    if (el.classList.contains(HOVERED_CLASS_NAME)) {
-		// Nothing to do
+		// We already processed this word, nothing to do here
 		return;
 	    }
 
-	    // console.log("Hovered", el.innerText, el);
+	    // Mark the word as "hovered"
 	    el.classList.add(HOVERED_CLASS_NAME);
 
+	    // Create a falling clone of the word
 	    makeFallingClone(el);
-	})
-    );
+	});
+
+	// Handle clicks on the word.
+	// You can do various stuff, but in this example:
+	// - A plain click restores the word to its original state
+	// - Ctrl+click creates another falling clone of the word
+	word.addEventListener("click", event => {
+	    let el = event.target;
+	    console.log("Click", event.ctrlKey);
+
+	    if (event.ctrlKey) {
+		makeFallingClone(el);
+	    }
+	    else {
+		el.classList.remove(HOVERED_CLASS_NAME);
+	    }
+
+	});
+    });
 
 })();
 
 
 function randomNumber(min, max) {
+    // Generate a random number within range
     return min + (Math.random() * (max - min));
 }
 
 
 function randomAnimationDuration(min, max) {
+    // Generate a random number in seconds
     return `${randomNumber(min, max)}s`;
 }
 
@@ -49,12 +82,42 @@ function makeFallingClone(element) {
     newElement.append(wordElem);
 
     newElement.addEventListener("animationend", event => {
-	// console.log(event);
+
+	// When the animation ends (and the element is outside the
+	// page), destroy the element.
 	if (event.animationName === "fallingWord") {
-	    // We're done -> remove it
 	    document.body.removeChild(newElement);
 	}
+
     });
+
     document.body.append(newElement);
-    // console.log("Falling clone", newElement);
+}
+
+
+function splitText(text) {
+    return text
+    // Split each line of text
+	.split(/\n/)
+    // Remove white space at the beginning / end of each line,
+    // then split each line into words.
+	.map(para => para.trim(/\s+/).split(/\s+/))
+    // Remove empty lines
+	.filter(para => para.length > 0);
+}
+
+
+function renderTextMarkup(text, container) {
+    let paras = splitText(text);
+    paras.forEach(para => {
+	let pEl = document.createElement("p");
+	para.forEach(word => {
+	    let wEl = document.createElement("span");
+	    wEl.classList.add("word")
+	    wEl.textContent = word;
+	    pEl.append(wEl);
+	    pEl.append(" ");
+	});
+	container.append(pEl);
+    });
 }
